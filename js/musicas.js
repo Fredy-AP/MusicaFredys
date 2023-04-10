a7c8b532b3bd8b7e7e2bc05126fee89cb45b8a9f
software development <103303538+fredyhack22@users.noreply.github.com>
2023-04-09 20:24:21 -0500

    Add files via upload

---
+++ js/musicas.js
@@ -0,0 +1,67 @@
+const songs=[
+  {
+    id: "song1",
+    name: "Dont be shy",
+    artist: "Karol G feat Dj Tiesto",
+    url: "assets/musicas/KarolG.mp3",
+    img: "assets/img/Karol.jpg"
+    // url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Another%20One%20Bites%20The%20Dust.mp3",
+    // img: "https://github.com/Brandon328/music-player/blob/master/assets/img/another%20one%20bites%20the%20dust.jpg"
+  },
+  {
+    id: "song2",
+    name: "Malissa",
+    artist: "Malissa feat Arcangel",
+    url: "assets/musicas/Malissa.mp3",
+    img: "assets/img/Arcangel.jpg"
+    // url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Antes%20De%20Los%20Veinte.mp3",
+    // img: "https://github.com/Brandon328/music-player/blob/master/assets/img/antes%20de%20los%2020.jpg"
+  },
+  {
+    id: "song3",
+    name: "Dangerous",
+    artist: "Michael Jackson",
+    url: "assets/musicas/Dangerous.mp3",
+    img: "assets/img/dangerous.jpg"
+    // url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Dangerous.mp3",
+    // img: "https://github.com/Brandon328/music-player/blob/master/assets/img/dangerous.jpg"
+  },
+  {
+    id: "song4",
+    name: "Elements",
+    artist: "Ludovico Einaudi",
+    url: "assets/musicas/Elements.mp3",
+    img: "assets/img/elements.jpg"
+    // url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Elements.mp3",
+    // img: "https://github.com/Brandon328/music-player/blob/master/assets/img/elements.jpg"
+  },
+  {
+    id: "song5",
+    name: "La mujer perfecta",
+    artist: "Kurt",
+    url: "assets/musicas/La Mujer Perfecta.mp3",
+    img: "assets/img/la mujer perfecta.jpg"
+    // url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/La%20Mujer%20Perfecta.mp3",
+    // img: "https://github.com/Brandon328/music-player/blob/master/assets/img/la%20mujer%20perfecta.jpg"
+  },
+  {
+    id: "song6",
+    name: "Demoralizer",
+    artist: "Municipal-wasted",
+    url: "assets/musicas/Demoralizer.mp3",
+    img: "assets/img/Demoralizer.jpeg"
+    // url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Todo%20Cambio.mp3",
+    // img: "https://github.com/Brandon328/music-player/blob/master/assets/img/todo%20cambio.jpg"
+  },
+  {
+    id: "song7",
+    name: "Todo de ti",
+    artist: "Rauw Alejandro",
+    url: "assets/musicas/Todo de ti.mp3",
+    img: "assets/img/todo de ti.jpg"
+    // url: "https://github.com/Brandon328/music-player/blob/master/assets/musicas/Todo%20de%20ti.mp3",
+    // img: "https://github.com/Brandon328/music-player/blob/master/assets/img/todo%20de%20ti.jpg"
+  }
+];
+
+
